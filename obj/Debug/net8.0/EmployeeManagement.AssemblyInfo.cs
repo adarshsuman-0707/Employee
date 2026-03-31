6fafd90d179bcb8731e68dc7e6a5969d57bb21b6
Adarsh suman <142964134+adarshsuman-0707@users.noreply.github.com>
2026-03-31 14:41:52 +0530

    Record

--- obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbe20d36b5a4c631127c09cf1bd61764214f498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb78b6a6e2467f60fd696225cec46cf8ac4046f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
