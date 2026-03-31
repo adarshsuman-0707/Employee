e35d5bf1cfb564218d8d5f7313a0ddd32fba9b7d
Adarsh suman <142964134+adarshsuman-0707@users.noreply.github.com>
2026-03-31 16:40:48 +0530

    Second commit

--- obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22965354db65af94c9a2c76129d67e43a21989be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173b017ad90848ee462e3cffd8a94b1554c36603")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
